6d2185a4e4949aeb1d906350f91e7b0e0156d0a1
Valen98 <leo.wahlandtcs@gmail.com>
2024-04-08 12:21:49 +0200

    Added a bar plot of the data

--- Main.py
+++ Main.py
@@ -18,12 +18,19 @@ def main():
         ld_data = json.load(f)
         ld_runs = ld_data['benchmarks'][0]['metrics']['timeToInitialDisplayMs']['runs']
         ld_low = ld_data['benchmarks'][0]['metrics']['timeToInitialDisplayMs']['minimum']
+        ld_median = ld_data['benchmarks'][0]['metrics']['timeToInitialDisplayMs']['median']
+        ld_max = ld_data['benchmarks'][0]['metrics']['timeToInitialDisplayMs']['maximum']
     with open(medium_demand) as f:
         md_data = json.load(f)
         md_runs = md_data['benchmarks'][0]['metrics']['timeToInitialDisplayMs']['runs']
+        md_low = md_data['benchmarks'][0]['metrics']['timeToInitialDisplayMs']['minimum']
+        md_median = md_data['benchmarks'][0]['metrics']['timeToInitialDisplayMs']['median']
+        md_max = md_data['benchmarks'][0]['metrics']['timeToInitialDisplayMs']['maximum']
     with open(high_demand) as f:
         hd_data = json.load(f)
         hd_runs = hd_data['benchmarks'][0]['metrics']['timeToInitialDisplayMs']['runs']
+        hd_low = hd_data['benchmarks'][0]['metrics']['timeToInitialDisplayMs']['minimum']
+        hd_median = hd_data['benchmarks'][0]['metrics']['timeToInitialDisplayMs']['median']
         hd_max = hd_data['benchmarks'][0]['metrics']['timeToInitialDisplayMs']['maximum']
 
     # Create a DataFrame from the runs data
@@ -35,6 +42,7 @@ def main():
     # Title for the boxplot depending on the UI
     if "XML" in path:
         plt.title('Boxplot of XML Startup time ' + path)
+
     else:
         plt.title('Boxplot of Jetpack Compose Startup Time ' + path)
 
@@ -42,14 +50,42 @@ def main():
 
     # Set the y-lim to be the high demand worst time + 1000.
     # Due to High demand always on our test have worse startup time.
+    '''
     plt.ylim(
         1250,
         1900
     )
+    '''
     plt.grid(True)
+    plt.show()
+
+    # Bar plot
+    barWidth = 0.25
+    fig = plt.subplots(figsize=(12, 8))
+
+    lowBar = [ld_low, ld_median, ld_max]
+    mediumBar = [md_low, md_median, md_max]
+    highBar = [hd_low, hd_median, hd_max]
+
+    br1 = np.arange(len(lowBar))
+    br2 = [x + barWidth for x in br1]
+    br3 = [x + barWidth for x in br2]
+
+    plt.bar(br1, lowBar, color='r', width=barWidth, edgecolor='grey', label='Low Demand')
+    plt.bar(br2, mediumBar, color='g', width=barWidth, edgecolor='grey', label='Medium Demand')
+    plt.bar(br3, highBar, color='b', width=barWidth, edgecolor='grey', label='High Demand')
+
+    # Title for the boxplot depending on the UI
+    if "XML" in path:
+        plt.xlabel("The startup time of XML " + path)
+
+    else:
+        plt.xlabel("The startup time of Jetpack Compose " + path)
+
+    plt.ylabel('Time (ms)')
+    plt.xticks([r + barWidth for r in range(len(lowBar))], ["Min", "Median", "Max"])
+    plt.legend()
     plt.show()
 
 
 main()
-
-
