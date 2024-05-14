a53ace31fcac0f9393944e627806dea09b04222d
Valen98 <leo.wahlandtcs@gmail.com>
2024-05-14 17:17:43 +0200

    Added calculation for each plot and saved the plots under /Plots/Calculation/

--- calculation.py
+++ calculation.py
@@ -1,5 +1,9 @@
 import json
 import math
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 def main():
@@ -9,17 +13,62 @@ def main():
         with open(path) as f:
             return json.load(f)
 
+    bar_width = 0.25
+
+    cpu_bar_list = {}
+    memory_bar_list = {}
+    battery_bar_list = {}
+    memory_bar_list_no_outliers = {}
+    battery_bar_list_no_outliers = {}
+
     all_data = read_json_file(path)
-    print(all_data)
     for data in all_data:
-            print(data)
-            for categories in all_data[data]:
-                mean = sum(all_data[data][categories]) / len(all_data[data][categories])
-                variance = sum([((x - mean) ** 2) for x in all_data[data][categories]]) / len(all_data[data][categories])
-                res = variance ** 0.5
-                z = 1.96
-                e = z * res / math.sqrt(len(all_data[data][categories]))
-                print("with category: " + categories + " E = " + str(round(e, 3)) + "\n")
+        print("\n", data)
+        for categories in all_data[data]:
+
+            match categories:
+                case "CPU":
+                    if 'noOutliers' not in data:
+                        cpu_bar_list[data] = all_data[data][categories]
+                case "battery":
+                    if 'noOutliers' not in data:
+                        battery_bar_list[data] = all_data[data][categories]
+                    else:
+                        battery_bar_list_no_outliers[data] = all_data[data][categories]
+                case "memory":
+                    if 'noOutliers' not in data:
+                        memory_bar_list[data] = all_data[data][categories]
+                    else:
+                        memory_bar_list_no_outliers[data] = all_data[data][categories]
+
+            mean = sum(all_data[data][categories]) / len(all_data[data][categories])
+            variance = sum([((x - mean) ** 2) for x in all_data[data][categories]]) / len(all_data[data][categories])
+            res = variance ** 0.5
+            z = 1.96
+            e = z * res / math.sqrt(len(all_data[data][categories]))
+            print("Mean value of " + categories + ": m =", round(mean, 3))
+            print("with category: " + categories + " E ", round(e, 3))
+
+    all_plots = {
+        "CPU": cpu_bar_list,
+        "Memory": memory_bar_list,
+        "Memory excluding semi-arbitrary outliers": memory_bar_list_no_outliers,
+        "Current": battery_bar_list,
+        "Current excluding semi-arbitrary outliers": battery_bar_list_no_outliers
+    }
+
+    print(all_plots)
+    for title, plot in all_plots.items():
+        for key, value in plot.items():
+            while len(value) < 30:
+                value.append(None)
+        df = pd.DataFrame(plot)
+        plt.figure(figsize=(15, 6))
+        sns.boxplot(data=df, width=0.5)
+        plt.title(title)
+        plt.grid(True)
+        plt.show()
+        plt.savefig("Plots/Calculation/" + title, format='svg')
 
 
 main()
